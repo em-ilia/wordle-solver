0e826a9cd37c302cef1d85609d2ce850aa82db98
Emilia <contact@emiliaallison.com>
2022-02-06 00:34:05 -0500

    Make header file for TotalLetterDict

--- stats/TotalLetterDict.cpp
+++ stats/TotalLetterDict.cpp
@@ -1,14 +1,11 @@
-#include "LetterDict.hpp"
+#include "TotalLetterDict.hpp"
 
-class TotalLetterDict: public LetterDict {
-	public:
-		static TotalLetterDict from_words(std::vector<std::string> words) {
-			TotalLetterDict tld = TotalLetterDict();
-			for (auto & word : words) {
-				for (auto letter : word) {
-					tld.inc(letter);
-				}
-			}
-			return tld;
+TotalLetterDict TotalLetterDict::from_words(std::vector<std::string> words) {
+	TotalLetterDict tld = TotalLetterDict();
+	for (auto & word : words) {
+		for (auto letter : word) {
+			tld.inc(letter);
 		}
-};
+	}
+	return tld;
+}
