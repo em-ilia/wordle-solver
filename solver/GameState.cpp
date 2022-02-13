7870cde09c48b60da03a02e09179f514e6d555a4
Emilia <contact@emiliaallison.com>
2022-02-12 19:26:53 -0500

    Lots of print statements and bug fixes
    Notably all of the std::remove_if calls needed an associated
    std::vector.erase call

--- solver/GameState.cpp
+++ solver/GameState.cpp
@@ -1,4 +1,5 @@
 #include "GameState.hpp"
+#include <iostream>
 
 
 GameState::GameState(std::vector<AugmentedWord> initialList) {
@@ -8,15 +9,23 @@ GameState::GameState(std::vector<AugmentedWord> initialList) {
 }
 
 void GameState::enter(std::string word, std::array<int, 5> res) {
+	//std::cout << "BEGIN CALL TO ENTER" << "\n";
 	for (int i = 0; i < 5; i++) {
 		if (res[i] == 0) {
+			//std::cout << "In enter, res is 0: disallowing " << word[i] << "\n";
 			addDisallowed(word[i]);
 		} else if (res[i] == 1) {
+			//std::cout << "In enter, res is 1" << "\n";
+			//std::cout << "Requiring " << word[i] << "\n";
+			//std::cout << "Adding to anti " << word[i] << " at pos " << std::to_string(i) << "\n";
 			addRequired(word[i]);
 			addAnti(i, word[i]);
 		} else if (res[i] == 2) {
+			//std::cout << "In enter, res is 2" << "\n";
 			addState(i, word[i]);
 			addRequired(word[i]);
+			//std::cout << "State is now: " << state << "\n";
+			//std::cout << "Requiring " << word[i] << "\n";
 		}
 	}
 
@@ -25,13 +34,20 @@ void GameState::enter(std::string word, std::array<int, 5> res) {
 											required.begin(), required.end(),
 											std::inserter(diff, diff.end()));
 	disallowed = diff;
+	//std::cout << "END CALL TO ENTER\n";
 }
 
 void GameState::recalculate() {
+	std::cout.setstate(std::ios_base::failbit); // DISABLE DEBUG IN RECALCULATE
+	std::cout << "\nBEGIN CALL TO RECALCULATE\n";
 	filter_disallowed();
 	filter_required();
 	filter_positional();
 	filter_anti();
+	std::cout << "END CALL TO RECALCULATE\n";
+	std::cout.clear();
+}
+
 std::vector<AugmentedWord> GameState::getWordList() {
 	return wl;
 }
@@ -63,40 +79,72 @@ void GameState::addState(int pos, char letter) {
 // smh
 
 void GameState::filter_disallowed() {
+	std::cout << "BEGIN CALL TO FILTER DISALLOWED\n";
+	std::cout << "Initial size of wl is " << std::to_string(wl.size()) << "\n";
 	for (char letter : disallowed) {
-		std::remove_if(wl.begin(), wl.end(),
+		std::cout << "Going to test " << letter << "\n";
+		wl.erase(std::remove_if(wl.begin(), wl.end(),
 				[letter](auto aug_word) {
-					return aug_word.word.find(letter) == std::string::npos;
-				});
+					//std::cout << "Is " << letter << " in " << aug_word.word << ": " <<
+					//std::to_string(aug_word.word.find(letter) != std::string::npos) << "\n";
+					return aug_word.word.find(letter) != std::string::npos;
+				}), wl.end());
 	}
+	std::cout << "Final size of wl is " << std::to_string(wl.size()) << "\n";
+	std::cout << "END CALL TO FILTER DISALLOWED\n";
 }
 
 void GameState::filter_required() {
+	std::cout << "BEGIN CALL TO FILTER REQUIRED\n";
+	std::cout << "Initial size of wl is " << std::to_string(wl.size()) << "\n";
 	for (char letter : required) {
-		std::remove_if(wl.begin(), wl.end(),
+		std::cout << "Going to test " << letter << "\n";
+		wl.erase(std::remove_if(wl.begin(), wl.end(),
 				[letter](auto aug_word) {
-					return aug_word.word.find(letter) != std::string::npos;
-				});
+					//std::cout << "Is " << letter << " in " << aug_word.word << ": " <<
+					//std::to_string(aug_word.word.find(letter) == std::string::npos) << "\n";
+					return aug_word.word.find(letter) == std::string::npos;
+				}), wl.end());
 	}
+	std::cout << "Final size of wl is " << std::to_string(wl.size()) << "\n";
+	std::cout << "END CALL TO FILTER REQUIRED\n";
 }
 
 void GameState::filter_positional() {
+	std::cout << "BEGIN CALL TO FILTER POSITIONAL\n";
+	std::cout << "Initial size of wl is " << std::to_string(wl.size()) << "\n";
+	std::cout << "Current state is " << state << "\n";
 	for (int i = 0; i < 5; i++) {
+		//std::cout << "About to test state pos " << std::to_string(i) << " which has value " <<
+		//	state[i] << "\n";
 		if (state[i] == '?') continue;
-		std::remove_if(wl.begin(),wl.end(),
+		wl.erase(std::remove_if(wl.begin(),wl.end(),
 				[this, i](auto aug_word) {
+					//std::cout << "Does " << aug_word.word[i] << " match " << state[i] << "?"
+					//<< std::to_string(aug_word.word[i] != this -> state[i]) << "\n";
 					return aug_word.word[i] != this -> state[i];
-				});
+				}), wl.end());
 	}
+	std::cout << "Final size of wl is " << std::to_string(wl.size()) << "\n";
+	std::cout << "END CALL TO FILTER POSITIONAL\n";
 }
 
 void GameState::filter_anti() {
+	std::cout << "BEGIN CALL TO FILTER ANTI\n";
+	std::cout << "Initial size of wl is " << std::to_string(wl.size()) << "\n";
 	for (auto antiPair : anti) {
+		//std::cout << "Going to test " << antiPair.first << " containing " <<
+		//	std::to_string(antiPair.second.size()) << " elements" << "\n";
 		for (auto pos: antiPair.second) {
-			std::remove_if(wl.begin(),wl.end(),
+			//std::cout << "Testing if " << antiPair.first << " at " << std::to_string(pos) << "\n";
+			wl.erase(std::remove_if(wl.begin(),wl.end(),
 					[antiPair, pos](auto aug_word) {
-						return aug_word.word[pos] = antiPair.first;
-					});
+						//std::cout << "Is " << aug_word.word[pos] << " equal to " << antiPair.first
+						//<< "?" << std::to_string(aug_word.word[pos] == antiPair.first) << "\n";
+						return aug_word.word[pos] == antiPair.first;
+					}), wl.end());
 		}
 	}
+	std::cout << "Final size of wl is " << std::to_string(wl.size()) << "\n";
+	std::cout << "END CALL TO FILTER ANTI\n";
 }
