3c7ba2e54595cf8fcdc563da9973afee4a3cd6ca
Emilia <contact@emiliaallison.com>
2022-02-12 19:50:03 -0500

    Test of GameState
    Cases 1,3, and 4 pass.
    Unknown issue with case 2, which may be related to printing?

--- solver/tester.cpp
+++ solver/tester.cpp
@@ -3,15 +3,79 @@
 #include "WORDS.hpp"
 #include <iostream>
 
+void printFive(std::vector<AugmentedWord> wl) {
+	/*
+	for (auto i = wl.end() - 5; i != wl.end(); i++) {
+		std::cout << formatAugmentedWord(*i) << " ";
+	}
+	*/
+	if (wl.size() == 0) return;
+	std::cout << formatAugmentedWord(wl.back());
+	std:: cout << "\n";
+}
+
 int main() {
+	std::cout << "TEST CASE 1:\n";
 	WordStats ws = WordStats(WORDS);
 	GameState game = GameState(ws.maximizeWordProb());
-	auto best = formatAugmentedWord(game.getWordList().back());
-	std::cout << best << "\n";
+	printFive(game.getWordList());
 
-	game.enter("slate", {0,2,1,1,0});
+	game.enter("slate", {0,0,0,0,0});
 	game.recalculate();
-	best = formatAugmentedWord(game.getWordList().back());
-	std::cout << std::to_string(game.getWordList().size()) << "\n";
-	std::cout << best << "\n";
+	printFive(game.getWordList());
+
+	game.enter("crony", {0,0,0,0,0});
+	game.recalculate();
+	printFive(game.getWordList());
+
+	game.enter("humid", {2,2,2,0,0});
+	game.recalculate();
+	printFive(game.getWordList());
+
+	std::cout << "\n";
+	std::cout << "TEST CASE 2:\n";
+
+	game = GameState(ws.maximizeWordProb());
+	printFive(game.getWordList());
+
+	game.enter("slate", {0,0,2,0,2});
+	game.recalculate();
+	printFive(game.getWordList());
+
+	game.enter("crane", {0,0,2,0,2});
+	game.recalculate();
+	printFive(game.getWordList());
+
+	game.enter("weave", {1,0,2,0,2});
+	game.recalculate();
+	printFive(game.getWordList());
+
+	std::cout << "\n";
+	std::cout << "TEST CASE 3:\n";
+
+	game = GameState(ws.maximizeWordProb());
+	printFive(game.getWordList());
+
+	game.enter("slate", {1,2,0,0,0});
+	game.recalculate();
+	printFive(game.getWordList());
+
+	std::cout << "\n";
+	std::cout << "TEST CASE 4:\n";
+
+	game = GameState(ws.maximizeWordProb());
+	printFive(game.getWordList());
+
+	game.enter("slate", {0,1,1,0,0});
+	game.recalculate();
+	printFive(game.getWordList());
+
+	game.enter("gaily", {0,1,0,1,0});
+	game.recalculate();
+	printFive(game.getWordList());
+
+	game.enter("coral", {1,2,0,2,2});
+	game.recalculate();
+	printFive(game.getWordList());
+
 }
